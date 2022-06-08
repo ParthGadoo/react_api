4703fd8fcbaab255fa1929045c28e677939b5bc2
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-06-08 16:15:33 +0530

    front end done

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(index.html);
+});
 
 app.use("/api/todos", todoRoutes);
 
